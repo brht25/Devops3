df3ba9d9a5d61d750c7dd7295e36fbad6e18d35b
brht25 <berhatyesilyurt@gmail.com>
2026-04-24 14:41:27 +0300

    fix: update Java version to 25 in CI/CD pipeline and use dynamic user path in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo systemctl start docker 2>/dev/null || true
 sudo docker pull brht13/devops3:latest
-sudo /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml down
-sudo /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d
+sudo /usr/local/bin/docker-compose -f /home/$USER/docker-compose.yml down
+sudo /usr/local/bin/docker-compose -f /home/$USER/docker-compose.yml up -d
