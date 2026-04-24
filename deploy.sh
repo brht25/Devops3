861dcd9d84a3707d392bf04582e82de995ea7ed3
brht25 <berhatyesilyurt@gmail.com>
2026-04-24 13:32:57 +0300

    Add sudo permissions to docker commands in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker pull brht13/devops3:latest
-docker compose -f /home/ec2-user/docker-compose.yml down
-docker compose -f /home/ec2-user/docker-compose.yml up -d
+sudo systemctl start docker 2>/dev/null || true
+sudo docker pull brht13/devops3:latest
+sudo docker-compose -f /home/ec2-user/docker-compose.yml down
+sudo docker-compose -f /home/ec2-user/docker-compose.yml up -d
