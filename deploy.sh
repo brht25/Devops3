d64dae5f2620c48493c2081524b25bebfc6a1910
brht25 <berhatyesilyurt@gmail.com>
2026-04-28 11:37:02 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo systemctl start docker 2>/dev/null || true
 sudo docker pull brht13/devops3:latest
-sudo /usr/local/bin/docker-compose -f /home/$USER/docker-compose.yml down
-sudo /usr/local/bin/docker-compose -f /home/$USER/docker-compose.yml up -d
+sudo /usr/local/bin/docker-compose -f /home/ubuntu/docker-compose.yml down
+sudo /usr/local/bin/docker-compose -f /home/ubuntu/docker-compose.yml up -d
