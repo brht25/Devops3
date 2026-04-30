0024ff28ae37c396637b1c3526de2cee186862b7
brht25 <berhatyesilyurt@gmail.com>
2026-04-30 08:46:21 +0300

    11

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -44,4 +44,8 @@ public class CategoryController {
         categoryService.deleteCategory(id);
         return ResponseEntity.noContent().build();
     }
+        @GetMapping("/version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok("v2 - CI/CD Pipeline Demo");
+    }
 }
